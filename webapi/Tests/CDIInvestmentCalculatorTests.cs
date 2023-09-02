ae138858f129b7b58c4e2a5a9b9fca07dd6bb062
gitfernandolopes <novavidafernandolopes@gmail.com>
2023-09-02 09:30:48 -0300

    #Test1

---
+++ webapi/Tests/CDIInvestmentCalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using CDBCalculationApi.Interfaces;
+using CDBCalculationApi.Models;
+using NUnit.Framework;
+
+namespace CDBCalculationApi.Tests
+{
+    [TestFixture]
+    public class CDIInvestmentCalculatorTests
+    {
+        [Test]
+        public void Calculate_CalculatesInvestmentCorrectly()
+        {
+
+            CDIInvestmentCalculator calculator = new CDIInvestmentCalculator();
+            InvestmentData data = new InvestmentData
+            {
+                InitialValue = 1000,
+                Months = 12
+            };
+
+            InvestmentResult result = calculator.Calculate(data);
+
+            Assert.That(result.GrossResult, Is.EqualTo(1261.58).Within(0.01)); 
+            Assert.That(result.NetResult, Is.EqualTo(1161.58).Within(0.01)); 
+        }
+
+        [Test]
+        public void Calculate_CalculatesInvestmentWithDifferentValues()
+        {
+            CDIInvestmentCalculator calculator = new CDIInvestmentCalculator();
+
+            InvestmentData data1 = new InvestmentData
+            {
+                InitialValue = 2000,
+                Months = 6
+            };
+
+            InvestmentData data2 = new InvestmentData
+            {
+                InitialValue = 5000,
+                Months = 24
+            };
+
+            InvestmentResult result1 = calculator.Calculate(data1);
+            InvestmentResult result2 = calculator.Calculate(data2);
+
+            Assert.That(result1.GrossResult, Is.EqualTo(2123.36).Within(0.01)); 
+            Assert.That(result1.NetResult, Is.EqualTo(1943.36).Within(0.01)); 
+
+            Assert.That(result2.GrossResult, Is.EqualTo(5456.83).Within(0.01));
+            Assert.That(result2.NetResult, Is.EqualTo(4996.83).Within(0.01));
+        }
+
+    }
+}
