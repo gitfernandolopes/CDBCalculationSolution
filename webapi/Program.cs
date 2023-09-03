9f70e0839df47403f8c3a8f094e283fc49787559
gitfernandolopes <novavidafernandolopes@gmail.com>
2023-09-03 14:29:03 -0300

    #Swagger

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,14 +1,27 @@
 using CDBCalculationApi.Interfaces;
 using CDBCalculationApi.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API de Cálculo do CDB",
+        Version = "v1",
+        Description = "Esta API permite calcular o rendimento bruto e líquido de um investimento em CDB com base em parâmetros fornecidos. " +
+                      "Os valores de entrada são:\n" +
+                      "- 'initialValue': O valor inicial do investimento em reais (deve ser um valor positivo).\n" +
+                      "- 'months': O prazo em meses para resgate do investimento (deve ser maior que 1).",
+    });
+});
 builder.Services.AddScoped<IInvestmentCalculator, CdiInvestmentCalculator>();
 
 var app = builder.Build();
@@ -17,7 +30,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CDBCalculationApi.v1");
+    });
 }
 
 app.UseHttpsRedirection();
@@ -27,3 +43,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
