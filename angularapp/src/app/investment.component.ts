eafc16d8b24dfb6348c76824f73bdd5824dd805c
gitfernandolopes <novavidafernandolopes@gmail.com>
2023-09-07 08:02:44 -0300

    #front

--- angularapp/src/app/investment.component.ts
+++ angularapp/src/app/investment.component.ts
@@ -6,23 +6,45 @@ import { InvestmentService } from './investment.service';
   templateUrl: './investment.component.html'
 })
 export class InvestmentComponent {
-  initialValue: number;
-  months: number;
-  grossResult: number;
-  netResult: number;
+  initialValue: number = 0;
+  months: number = 0;
+  grossResult: number = 0;
+  netResult: number = 0;
+  errorMessage: string = '';
 
-  constructor(private investmentService: InvestmentService) {
-    this.initialValue = 0;
-    this.months = 0;
-    this.grossResult = 0;
-    this.netResult = 0;
-  }
+  constructor(private investmentService: InvestmentService) { }
 
   calculateInvestment(): void {
-    this.investmentService.calculateInvestment(this.initialValue, this.months)
-      .subscribe(result => {
-        this.grossResult = result.grossResult;
-        this.netResult = result.netResult;
+    if (!this.isValidInput(this.initialValue) || !this.isValidInput(this.months)) {
+      this.errorMessage = 'Only numbers are allowed.';
+      return;
+    }
+
+    const initialValue = this.initialValue;
+    const months = this.months;
+
+    this.investmentService.calculateInvestment(initialValue, months)
+      .subscribe({
+        next: (result) => {
+          this.grossResult = result.grossResult;
+          this.netResult = result.netResult;
+          this.errorMessage = '';
+        },
+        error: (error) => {
+          this.errorMessage = error;
+        }
       });
+  }
+
+  isValidInput(input: number): boolean {
+    return !isNaN(input) && isFinite(input) && input >= 0;
+  }
+
+  onInitialValueChange(value: number): void {
+    this.initialValue = value;
+  }
+
+  onMonthsChange(value: number): void {
+    this.months = value;
   }
 }
--- angularapp/src/app/investment.service.ts
+++ angularapp/src/app/investment.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,18 @@ export class InvestmentService {
   calculateInvestment(initialValue: number, months: number): Observable<any> {
     const data = { InitialValue: initialValue, Months: months };
     return this.http.post('/api/calculation/calculate', data)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          const errorMessage = 'Invalid input data!';
+          return new Observable<any>((observer) => {
+            observer.error(errorMessage);
+          });
+        }),
+        map((response: any) => {
+          response.grossResult = parseFloat(response.grossResult.toFixed(2));
+          response.netResult = parseFloat(response.netResult.toFixed(2));
+          return response;
+        })
+      );
   }
 }
